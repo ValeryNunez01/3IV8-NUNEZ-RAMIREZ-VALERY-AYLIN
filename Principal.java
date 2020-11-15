26deba2703798b8ec6583a24274281eff0ef0e55
ValeryNunez01 <72088086+ValeryNunez01@users.noreply.github.com>
2020-11-15 16:59:39 -0600

    Ejercicio1-15/11/20-Núñez Valery
    Ejercicio1-15/11/20-Núñez Valery

---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal{
+    public static void main(String[] args) {
+     
+        Edad llamar = new Edad();
+        System.out.println("\nMade by Valery Ayin Nunez Ramirez - 3IV8");
+        //Se llama al metodo creado en la clase Edad
+        llamar.ingresar();
+
+    }
+}
